78ad71076ee92c9f610cf820d9c770d3b144b0c8
ViniciusMartins <viniciuslemosmartins1@gmail.com>
2024-12-11 21:25:44 -0300

    feat: create medical records context

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,39 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, FlatList, StyleSheet } from 'react-native';
 import Header from '@/app/components/header';
 import SearchBar from '@/app/components/searchBar';
 import CreateButton from '@/app/components/createButton';
 import RecordCard from '@/app/components/recordCard';
 import { CalendarRangeButton } from '../components/calendarButton';
-
-const data = [
-  {
-    id: '1',
-    name: 'Hospital Albert Einstein',
-    lastReview: 'sun oct 20. 12PM',
-    reviews: 4,
-  },
-  {
-    id: '2',
-    name: 'Hospital Sírio Libanês',
-    lastReview: 'sun oct 20. 12PM',
-    reviews: 4,
-  },
-  {
-    id: '3',
-    name: 'Hospital São Luiz',
-    lastReview: 'sun oct 20. 12PM',
-    reviews: 4,
-  },
-  {
-    id: '4',
-    name: 'Hospital Santa Isabel',
-    lastReview: 'sun oct 20. 12PM',
-    reviews: 4,
-  },
-];
+import { RecordContext } from '@/contexts/records';
+import { RecordContextManager } from '@/domain/interfaces/record-props';
 
 export default function AllRecordsScreen() {
+  const { records } = useContext(RecordContext) as RecordContextManager;
+
   return (
     <View style={styles.container}>
       <View style={{ width: '85%' }}>
@@ -47,13 +24,13 @@ export default function AllRecordsScreen() {
         </View>
       </View>
       <FlatList
-        data={data}
-        keyExtractor={(item) => item.id}
+        data={records}
+        keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <RecordCard
             name={item.name}
             lastReview={item.lastReview}
-            reviews={item.reviews}
+            reviews={item.totalReviews}
           />
         )}
         showsVerticalScrollIndicator={false}
